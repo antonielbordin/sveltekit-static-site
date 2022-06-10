c9ff6ab78d4232919ec49f09335fea1bfaee6fc7
antonielbordin <antonielbordin@hotmail.com>
2022-06-10 11:40:14 -0300

    Test deploy github pages

--- svelte.config.js
+++ svelte.config.js
@@ -6,13 +6,11 @@ const config = {
     adapter: adapter(),
     prerender: {
       default: true,
-    }
-    /*
+    },    
     // enable this base url for publish in github pages
     paths: {      
       base: process.env.NODE_ENV === "production" ? "/sveltekit-static-site" : "",
-    },
-    */
+    },    
   },
 };
 
